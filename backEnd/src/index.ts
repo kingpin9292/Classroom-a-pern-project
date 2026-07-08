6d709a5f170c411d728f6399c4b2b14feae8c2ca
kingpin9292 <jakarea9292@gmail.com>
2026-07-07 22:44:21 -0400

    Implement subjects schema &  routes

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import subjectRouter from "./routes/subjects.js";
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use("/api/subjects", subjectRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
