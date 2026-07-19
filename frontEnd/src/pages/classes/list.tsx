1c3a776d68f9f716331a9e32c5000c6f8a04fda5
kingpin9292 <jakarea9292@gmail.com>
2026-07-19 00:09:19 -0400

    Implement Classes route & Classes page

--- frontEnd/src/pages/classes/list.tsx
+++ frontEnd/src/pages/classes/list.tsx
@@ -1,6 +1,182 @@
-import React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/components/ui/badge";
+import { useMemo, useState } from "react";
+import { ShowButton } from "@/components/refine-ui/buttons/show";
+import { useList } from "@refinedev/core";
+import { Subject, User } from "@/types";
 
-const list = () => {
+type ClassListItem = {
+  id: number;
+  name: string;
+  status: "active" | "inactive";
+  bannerUrl?: string;
+  subject?: {
+    name: string;
+  };
+  teacher?: {
+    name: string;
+  };
+  capacity: number;
+};
+
+const ClassesList = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedSubject, setSelectedSubject] = useState<string>("all");
+  const [selectedTeacher, setSelectedTeacher] = useState<string>("all");
+
+  const classColumn = useMemo<ColumnDef<ClassListItem>[]>(
+    () => [
+      {
+        id: "banner",
+        accessorKey: "bannerUrl",
+        size: 120,
+        header: () => <p className="column-title ml-2">Banner</p>,
+        cell: ({ getValue }) => {
+          const bannerUrl = getValue<string>();
+
+          return bannerUrl ? (
+            <img src={bannerUrl} alt="Class banner" className="ml-2 h-10 w-10 rounded-md object-cover" loading="lazy" />
+          ) : (
+            <span className="text-muted-foreground ml-2">No Image</span>
+          );
+        },
+      },
+      {
+        id: "name",
+        accessorKey: "name",
+        size: 220,
+        header: () => <p className="column-title">Class Name</p>,
+        cell: ({ getValue }) => {
+          const className = getValue<string>();
+
+          return <span className="text-foreground">{className}</span>;
+        },
+      },
+      {
+        id: "status",
+        accessorKey: "status",
+        size: 140,
+        header: () => <p className="column-title">Status</p>,
+        cell: ({ getValue }) => {
+          const status = getValue<"active" | "inactive">();
+          const variant = status === "active" ? "default" : "secondary";
+
+          return <Badge variant={variant}>{status}</Badge>;
+        },
+      },
+      {
+        id: "subject",
+        accessorKey: "subject.name",
+        size: 200,
+        header: () => <p className="column-title">Subject</p>,
+        cell: ({ getValue }) => {
+          const subjectName = getValue<string>();
+
+          return subjectName ? (
+            <Badge variant="secondary">{subjectName}</Badge>
+          ) : (
+            <span className="text-muted-foreground">Not set</span>
+          );
+        },
+      },
+
+      {
+        id: "teacher",
+        accessorKey: "teacher.name",
+        size: 200,
+        header: () => <p className="column-title">Teacher</p>,
+        cell: ({ getValue }) => {
+          const teacherName = getValue<string>();
+
+          return teacherName ? (
+            <span className="text-foreground">{teacherName}</span>
+          ) : (
+            <span className="text-muted-foreground">Not assigned</span>
+          );
+        },
+      },
+      {
+        id: "capacity",
+        accessorKey: "capacity",
+        size: 120,
+        header: () => <p className="column-title">Capacity</p>,
+        cell: ({ getValue }) => {
+          const capacity = getValue<number>();
+
+          return <span className="text-foreground">{capacity}</span>;
+        },
+      },
+
+      {
+        id: "details",
+        size: 140,
+        header: () => <p className="column-title">Details</p>,
+        cell: ({ row }) => {
+          <ShowButton resource="classes" recordItemId={row.original.id} variant="outline" className="sm">
+            View
+          </ShowButton>;
+        },
+      },
+    ],
+    [],
+  );
+
+  const { query: subjectsQuery } = useList<Subject>({
+    resource: "subjects",
+    pagination: {
+      pageSize: 100,
+    },
+  });
+
+  const { query: teachersQuery } = useList<User>({
+    resource: "users",
+    filters: [
+      {
+        field: "role",
+        operator: "eq",
+        value: "teacher",
+      },
+    ],
+    pagination: {
+      pageSize: 100,
+    },
+  });
+
+  const subjects = subjectsQuery.data?.data || [];
+  const teachers = teachersQuery.data?.data || [];
+
+  const subejectFilters =
+    selectedSubject === "all"
+      ? []
+      : [
+          {
+            field: "subject",
+            operator: "eq",
+            value: selectedSubject,
+          },
+        ];
+
+  const teacherFilters =
+    selectedTeacher === "all"
+      ? []
+      : [
+          {
+            field: "teacher",
+            operator: "eq" as const,
+            value: selectedTeacher,
+          },
+        ];
+
+  const searchFilters = searchQuery
+    ? [
+        {
+          field: "name",
+          operator: "contains" as const,
+          value: searchQuery,
+        },
+      ]
+    : [];
+
   return <div>list</div>;
 };
 
